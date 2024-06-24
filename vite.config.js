1e1a218794dcc5cb8939c7f55051fdd3d5484e1d
yuriy17 <yuriy_sh@zoho.com>
2024-06-24 23:27:10 +0300

    fix env import

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
-import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
+import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 import { compile } from 'ejs';
 import { readFile } from 'node:fs/promises';
@@ -9,77 +8,85 @@ const __filename = fileURLToPath(import.meta.url);
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 const __dirname = path.dirname(__filename);
-// Load environment variables from .env file
-dotenv.config();
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '');
 
-export default defineConfig({
-  plugins: [
-    import.meta.env.PROD &&
-      viteStaticCopy({
-        targets: [
-          {
-            // copy for dynamic import ejs
-            src: path.resolve(__dirname, 'src/templates/components/input.ejs'),
-            dest: 'assets/templates/components',
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [
+      mode === 'production' &&
+        viteStaticCopy({
+          targets: [
+            {
+              // copy for dynamic import ejs
+              src: path.resolve(__dirname, 'src/templates/components/input.ejs'),
+              dest: 'assets/templates/components',
+            },
+          ],
+        }),
+      // render in html ejs templates
+      ViteEjsPlugin(
+        // { title: 'My vue project!' },
+        {
+          ejs: (viteConfig) => {
+            // viteConfig is the current viteResolved config.
+            return {
+              root: viteConfig.root,
+              beautify: true,
+              // domain: 'example.com',
+            };
           },
-        ],
-      }),
-    // render in html ejs templates
-    ViteEjsPlugin(
-      // { title: 'My vue project!' },
-      {
-        ejs: (viteConfig) => {
-          // viteConfig is the current viteResolved config.
-          return {
-            root: viteConfig.root,
-            beautify: true,
-            // domain: 'example.com',
-          };
-        },
-      }
-    ),
-    // render in js ejs templates
-    // https://medium.com/@koistya/using-ejs-with-vite-7502a4f79e44
-    {
-      name: 'ejs',
-      async transform(_, id) {
-        if (id.endsWith('.ejs')) {
-          const src = await readFile(id, 'utf-8');
-          const code = compile(src, {
-            client: true,
-            strict: true,
-            localsName: 'env',
-            async: 'true',
-            // views: [path.resolve(__dirname, 'views')],
-            filename: path.relative(__dirname, id),
-          }).toString();
-          return `export default ${code}`;
         }
+      ),
+      // render in js ejs templates
+      // https://medium.com/@koistya/using-ejs-with-vite-7502a4f79e44
+      {
+        name: 'ejs',
+        async transform(_, id) {
+          if (id.endsWith('.ejs')) {
+            const src = await readFile(id, 'utf-8');
+            const code = compile(src, {
+              client: true,
+              strict: true,
+              localsName: 'env',
+              async: 'true',
+              // views: [path.resolve(__dirname, 'views')],
+              filename: path.relative(__dirname, id),
+            }).toString();
+            return `export default ${code}`;
+          }
+        },
+      },
+    ],
+    build: {
+      emptyOutDir: false,
+      outDir: path.join(__dirname, 'dist'),
+      rollupOptions: {
+        input: {
+          index: path.resolve(__dirname, 'src/index.html'),
+          booking: path.resolve(__dirname, 'src/booking/index.html'),
+        },
+        output: {
+          // entryFileNames: `[name].js`,
+          // chunkFileNames: `[name].js`,
+          // assetFileNames: `[name].[ext]`
+        },
       },
     },
-  ],
-  build: {
-    emptyOutDir: false,
-    outDir: path.join(__dirname, 'dist'),
-    rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, 'src/index.html'),
-        booking: path.resolve(__dirname, 'src/booking/index.html'),
-      },
-      output: {
-        // entryFileNames: `[name].js`,
-        // chunkFileNames: `[name].js`,
-        // assetFileNames: `[name].[ext]`
+    root: 'src',
+    server: {
+      port: env.VITE_PORT || 5173, // Use the port from the .env file or default to 5173
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {},
       },
     },
-  },
-  root: 'src',
-  server: {
-    port: import.meta.env.VITE_PORT || 5173, // Use the port from the .env file or default to 5173
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {},
-    },
-  },
+  };
 });
