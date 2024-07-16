542da940d5a016159f1e9fc76e8d28c7ba1f8b1e
Yuriy17 <yuriy_sh@zoho.com>
2024-07-16 22:25:32 +0300

    added static path

--- src/assets/js/utils/loadingIcon.js
+++ src/assets/js/utils/loadingIcon.js
@@ -1,7 +1,7 @@
 export function showLoadingIcon(container) {
   const loadingContainer = container.querySelector('.loading-container');
   if (loadingContainer) {
-    loadingContainer.innerHTML = `<img src="/images/loading.svg" alt="Loading..." class="loading-icon">`;
+    loadingContainer.innerHTML = `<img src="/static/images/loading.svg" alt="Loading..." class="loading-icon">`;
     loadingContainer.classList.remove('hidden');
   }
 }
