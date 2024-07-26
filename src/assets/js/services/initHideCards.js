89deae5dfbaf7edc61922c1c875cb86525d0a42a
Yuriy17 <yuriy_sh@zoho.com>
2024-07-26 16:30:12 +0300

    show hide cards

--- src/assets/js/services/initHideCards.js
+++ src/assets/js/services/initHideCards.js
@@ -1,35 +1,40 @@
-import { gridBreakpoints } from "../helpers/constants";
+import { gridBreakpoints } from '../helpers/constants';
 
 export const initHideCards = () => {
   const cardsElements = document.querySelectorAll('.show-more-cards');
-  if(cardsElements && cardsElements.length) {
-    cardsElements.forEach((cardsElement) => {
-      initShowHide({ cardsElement });
-      const showMoreButton = cardsElement.parentElement.querySelector('.show-more');
-
-      showMoreButton.addEventListener('click', (e) => {
-        cardsElement.dataset.hide = 'false';
-        initShowHide({ cardsElement });
-        e.currentTarget.style.display = 'none'
-      });
-    });
+  if (cardsElements && cardsElements.length) {
+    cardsElements.forEach((cardsElement) => initShowHide({ cardsElement }));
   }
-}
+};
 
 const initShowHide = ({ cardsElement }) => {
-  const cards = [...cardsElement.querySelectorAll('.card')];
-  const isHide = cardsElement.dataset.hide === 'true';
-  const maxInitCount = innerWidth > gridBreakpoints.md ? cardsElement.dataset.desktopCount : cardsElement.dataset.mobileCount;;
-  const showMoreText = cardsElement.parentElement.querySelector('.show-more-text');
-  showMoreText.innerText = `You're viewed ${isHide ? maxInitCount : cards.length} of ${cards.length} country`;
-  
-  for (let index = 0; index < cards.length; index++) {
-    const card = cards[index];
-    console.log("🚀 ~ initShowHide ~ cardsElement.dataset.hide === 'false':", );
-    if (index < maxInitCount || !isHide) {
-      card.style.display = 'flex';
+  const cardsFragment = new DocumentFragment();
+  const showHide = () => {
+    const cards = [...cardsElement.querySelectorAll('.card')];
+    const isHide = cardsElement.dataset.hide === 'true';
+    const maxInitCount = innerWidth > gridBreakpoints.md ? cardsElement.dataset.desktopCount : cardsElement.dataset.mobileCount;
+    const showMoreText = cardsElement.parentElement.querySelector('.show-more-text');
+
+    if (isHide) {
+      for (let index = 0; index < cards.length; index++) {
+        const card = cards[index];
+        if (index >= maxInitCount) {
+          cardsFragment.appendChild(card.cloneNode(true));
+          card.remove();
+        }
+      }
     } else {
-      card.style.display = 'none';
+
+      cardsElement.append(...cardsFragment.children);
     }
-  }
+    showMoreText.innerText = `You're viewed ${isHide ? maxInitCount : cards.length} of ${cards.length} country`;
+  };
+  showHide();
+  const showMoreButton = cardsElement.parentElement.querySelector('.show-more');
+
+  showMoreButton.addEventListener('click', (e) => {
+    cardsElement.dataset.hide = 'false';
+    showHide({ cardsElement, cardsFragment });
+    e.currentTarget.style.display = 'none';
+  });
 };
